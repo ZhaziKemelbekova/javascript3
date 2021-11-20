da1020e0606b224508f625f60f929033b16c1021
zhaziKemelbekova <zhazgulukemelbekova@gmail.com>
2021-11-21 00:26:47 +0400

    javascript

---
+++ script/script.js
@@ -0,0 +1,58 @@
+// const num = 10
+// //
+// // console.log(num)
+// //
+// // let a = 10
+// // let b = 2
+// // let c = 5
+// //
+// // console.log(a +b + c)
+// //
+// // const e = 100
+// //
+// // console.log(e * 2)
+// //
+// // const f = 67
+// //  console.log(67 % 7)
+// //
+// // let s = 10
+// // let g = 2
+// //
+// // h = s + g
+// // j = s - g
+// //  k  = s * g
+// // l  = s / g
+// //
+// // console.log (h)
+// // console.log (j)
+// // console.log(k)
+// // console.log(l)
+// //
+// // let p = 15
+// // let d =2
+// // const result = (p + d)
+// //
+// // console.log(result)
+
+// let a = 30
+// let b = 15
+// let c =  a + b
+// console.log(45 % 4)
+
+// let a = 17
+// let b = 10
+// let c = a - b
+// console.log(c)
+// let d = 12
+// const result = c + d
+// console.log(result)
+
+// let a = "10"
+// let b = "99"
+// let result = a + b
+// console.log( result)
+
+const name = "30"
+const num = 10
+const result = name + num
+console.log(result)
